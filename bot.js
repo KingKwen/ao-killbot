6e2888841f10e2e9e7a89e997d1795a8026d8342
KingKwen <37223793+KingKwen@users.noreply.github.com>
2018-03-10 17:41:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-// Define static constants
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -22,5 +19,65 @@ client.on('message', message => {
   	}
 });
 
+/**
+ * Wait until ready and logged in
+ * If we do not wait for the ready event
+ * All commands will process before we are authorized
+ */
+client.on('ready', () => {
+    console.log('Ready and waiting!');
+
+    // Set 'Playing Game' in discord
+    client.user.setGame('Albion Online'); // broken due to discord API changes
+
+    //fetchKills();
+
+    // Fetch kills every 30s
+    var timer = setInterval(function() {
+        //fetchKills();
+    }, 30000);
+});
+
+/**
+ * On receive message
+ */
+client.on('message', message => {
+    if (message.content.indexOf('!') !== 0 || message.author.bot) return;
+    else { // Execute command!
+        var args = message.content.slice('!'.length).trim().split(/ +/g);
+        var command = args.shift().toLowerCase();
+
+        // Test Command - !ping
+        if (command === 'ping') {
+            message.reply('pong');
+        }
+
+        else if (command === 'kbinfo') {
+            request({
+                json: true,
+                uri: 'https://gameinfo.albiononline.com/api/gameinfo/events/' + args[0]
+            }, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    //postKill(body, message.channel.id);
+                } else {
+                    console.log('Error: ', error); // Log the error
+                }
+            });
+        }
+
+        // [ADMIN] - clear config.botChannel messages
+        else if (command === 'kbclear') {
+            if (config.admins.includes(message.author.id) && message.channel.id == config.botChannel) {
+                message.channel.send('Clearing Killboard').then(msg => {
+                    msg.channel.fetchMessages().then(messages => {
+                        message.channel.bulkDelete(messages);
+                        console.log("[ADMIN] " + message.author.username + " cleared Killboard");
+                    })
+                })
+            }
+        }
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
-client.login('NDIxNTg1MDI3NDMyODQxMjE2.DYWS5A.5LXXi_Anifz_AZqDvCHCoGQ96Dg');
+client.login(process.env.BOT_TOKEN);
